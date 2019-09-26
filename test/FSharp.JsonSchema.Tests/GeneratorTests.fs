4ae7407dc5447754e1cb278177d1e782e0cfb7c7
Ryan Riley <ryan@tachyus.com>
2019-09-26 17:09:13 -0500

    Add serialization tests

--- test/FSharp.JsonSchema.Tests/GeneratorTests.fs
+++ test/FSharp.JsonSchema.Tests/GeneratorTests.fs
@@ -4,8 +4,8 @@ open Expecto
 
 [<Tests>]
 let tests =
-  testList "generator" [
-    test "I am (should fail)" {
-      "╰〳 ಠ 益 ಠೃ 〵╯" |> Expect.equal true false
-    }
-  ]
+    testList "generator" [
+        test "I am (should pass)" {
+            "╰〳 ಠ 益 ಠೃ 〵╯" |> Expect.equal true true
+        }
+    ]
--- test/FSharp.JsonSchema.Tests/JsonSerializationTests.fs
+++ test/FSharp.JsonSchema.Tests/JsonSerializationTests.fs
@@ -1,6 +1,7 @@
 module JsonSerializationTests
 
 open Newtonsoft.Json
+open Newtonsoft.Json.FSharp.Idiomatic
 open Expecto
 
 type Test =
@@ -8,12 +9,41 @@ type Test =
     | WithOneField of int
     | WithNamedFields of name:string * value:float
 
+let settings =
+    JsonSerializerSettings(
+        Converters=[|OptionConverter(); MultiCaseDuConverter("tag")|],
+        ContractResolver=Serialization.CamelCasePropertyNamesContractResolver())
+
 [<Tests>]
 let tests =
     testList "json serialization" [
         test "Option.None should serialize as null" {
             let expected = "null"
-            let actual = "null"
-            Expect.equal actual expected "Expected serializer to serialize Option.None as null"
+            let actual = JsonConvert.SerializeObject(None, settings)
+            Expect.equal actual expected "Expected serializer to use OptionConverter"
+        }
+
+        test "Option.Some(1) should serialize as 1" {
+            let expected = "1"
+            let actual = JsonConvert.SerializeObject(Some 1, settings)
+            Expect.equal actual expected "Expected serializer to use OptionConverter"
+        }
+
+        test "Test.Case should serialize as {\"tag\":\"Case\"}" {
+            let expected = """{"tag":"Case"}"""
+            let actual = JsonConvert.SerializeObject(Case, settings)
+            Expect.equal actual expected "Expected serializer to use OptionConverter"
+        }
+
+        test "Test.WithOneField(1) should serialize as {\"tag\":\"WithOneField\",\"Item\":1}" {
+            let expected = """{"tag":"WithOneField","Item":1}"""
+            let actual = JsonConvert.SerializeObject(WithOneField 1, settings)
+            Expect.equal actual expected "Expected serializer to use OptionConverter"
+        }
+
+        test "Test.WithNamedFields(\"name\", 1.) should serialize as {\"tag\":\"WithOneField\",\"name\":\"name\",\"value\":1.0}" {
+            let expected = """{"tag":"WithNamedFields","name":"name","value":1.0}"""
+            let actual = JsonConvert.SerializeObject(WithNamedFields("name", 1.), settings)
+            Expect.equal actual expected "Expected serializer to use OptionConverter"
         }
     ]
