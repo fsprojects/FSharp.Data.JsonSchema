d29626bdba9c91ebd168e0e7fd0ecaa666328a86
Ryan Riley <ryan@tachyus.com>
2019-09-26 17:55:44 -0500

    Initial implementation of JSchema for option and multi-case DU

--- src/FSharp.JsonSchema/Generator.fs
+++ src/FSharp.JsonSchema/Generator.fs
@@ -1,10 +1,53 @@
 ﻿module FSharp.JsonSchema.Generator
 
 open System
-open System.Reflection
+open System.Collections.Generic
 open Microsoft.FSharp.Reflection
 open Newtonsoft.Json
 open Newtonsoft.Json.Schema
 
-let generate (ty:Type) =
-    raise (NotImplementedException())
+type OptionGenerationProvider() =
+    inherit Generation.JSchemaGenerationProvider()
+
+    override __.GetSchema(context:Generation.JSchemaTypeGenerationContext) =
+        let unionType = context.ObjectType
+        let cases = FSharpType.GetUnionCases(unionType)
+        let schema = JSchema(Type=Nullable JSchemaType.Object)
+        for case in cases do
+            match case.Name with
+            | "None" ->
+                schema.AnyOf.Add(JSchema(Type=Nullable JSchemaType.Null))
+            | _ ->
+                let field = case.GetFields() |> Array.head
+                let propSchema = context.Generator.Generate(field.PropertyType)
+                schema.AnyOf.Add(propSchema)
+        schema
+
+type MultiCaseDuGenerationProvider(?casePropertyName) =
+    inherit Generation.JSchemaGenerationProvider()
+
+    let casePropertyName = defaultArg casePropertyName "kind"
+
+    override __.GetSchema(context:Generation.JSchemaTypeGenerationContext) =
+        let unionType = context.ObjectType
+        let cases = FSharpType.GetUnionCases(unionType)
+        let schema = JSchema(Type=Nullable JSchemaType.Object)
+        for case in cases do
+            let propSchema = JSchema(Type=Nullable JSchemaType.Object)
+            propSchema.Properties.Add(casePropertyName, JSchema(Type=Nullable JSchemaType.String))
+            let fields = case.GetFields()
+            for field in fields do
+                let fieldSchema = context.Generator.Generate(field.PropertyType)
+                propSchema.Properties.Add(KeyValuePair(field.Name, fieldSchema))
+            schema.AnyOf.Add(propSchema)
+        schema
+
+let generate casePropertyName (ty:Type) =
+    let generator =
+        Generation.JSchemaGenerator(
+            ContractResolver=Serialization.CamelCasePropertyNamesContractResolver())
+
+    // Create generation providers for various types, e.g. Option and MultiCaseDu?
+    generator.GenerationProviders.Add(Generation.StringEnumGenerationProvider())
+    generator.GenerationProviders.Add(OptionGenerationProvider())
+    generator.GenerationProviders.Add(MultiCaseDuGenerationProvider(casePropertyName))
