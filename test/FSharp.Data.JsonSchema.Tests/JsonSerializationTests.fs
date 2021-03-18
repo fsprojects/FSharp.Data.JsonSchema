521ce59ccdab29dd51c1065c3e0d1145f3a9a62c
Ryan Riley <ryanriley@live.com>
2021-03-18 11:17:32 -0500

    Add tuple and roundtrip serialization tests

--- test/FSharp.Data.JsonSchema.Tests/JsonSerializationTests.fs
+++ test/FSharp.Data.JsonSchema.Tests/JsonSerializationTests.fs
@@ -1,5 +1,6 @@
 module FSharp.Data.JsonSchema.Tests.JsonSerializationTests
 
+open System
 open FSharp.Data
 open Expecto
 
@@ -12,15 +13,57 @@ let tests =
             Expect.equal actual expected "Expected serializer to convert None to null"
         }
 
+        test "Option.None should roundtrip" {
+            let expected = None
+            let actual = Json.Deserialize(Json.Serialize(expected, "tag"), "tag")
+            Expect.equal actual expected "Expected serializer to convert None to null"
+        }
+
         test "Option.Some(1) should serialize as 1" {
             let expected = "1"
             let actual = Json.Serialize(Some 1, "tag")
             Expect.equal actual expected "Expected serializer to convert option to unwrapped value"
         }
 
+        test "Option.Some(1) should roundtrip" {
+            let expected = Some 1
+            let actual = Json.Deserialize(Json.Serialize(expected, "tag"), "tag")
+            Expect.equal actual expected "Expected serializer to convert option to unwrapped value"
+        }
+
+        test "tuple should serialize as array" {
+            let expected = """["2021-03-01T00:00:00",10.01]"""
+            let actual = Json.Serialize((DateTime(2021,3,1), 10.01))
+            Expect.equal actual expected "Expected serializer to convert tuple to array"
+        }
+
+        test "tuple should roundtrip" {
+            let expected = (DateTime(2021,3,1), 10.01)
+            let actual = Json.Deserialize(Json.Serialize(expected))
+            Expect.equal actual expected "Expected serializer to convert tuple to array"
+        }
+
+        test "array of tuples should serialize as array of arrays" {
+            let expected = """[["2021-03-01T00:00:00",10.01]]"""
+            let actual = Json.Serialize([|DateTime(2021,3,1), 10.01|])
+            Expect.equal actual expected "Expected serializer to convert array of tuple to array of arrays"
+        }
+
+        test "array of tuples should roundtrip" {
+            let expected = [|DateTime(2021,3,1), 10.01|]
+            let actual = Json.Deserialize(Json.Serialize(expected))
+            Expect.equal actual expected "Expected serializer to convert array of tuple to array of arrays"
+        }
+
         test "TestDU.Case should serialize as \"Case\"" {
             let expected = "\"Case\""
             let actual = Json.Serialize(Case, "tag")
+            Expect.equal actual expected "Expected serializer to convert union case with no fields to untagged string literal"
+        }
+
+        test "TestDU.Case should roundtrip" {
+            let expected = Case
+            let actual = Json.Deserialize(Json.Serialize(expected, "tag"), "tag")
             Expect.equal actual expected "Expected serializer to convert union case with no fields to untagged string literal"
         }
 
@@ -30,9 +73,21 @@ let tests =
             Expect.equal actual expected "Expected serializer to convert union case with unnamed fields to object with tag named \"tag\" with unnamed field to Item1"
         }
 
+        test "TestDU.WithOneField(1) should roundtrip" {
+            let expected = WithOneField 1
+            let actual = Json.Deserialize(Json.Serialize(expected, "tag"), "tag")
+            Expect.equal actual expected "Expected serializer to convert union case with unnamed fields to object with tag named \"tag\" with unnamed field to Item1"
+        }
+
         test "TestDU.WithNamedFields(\"name\", 1.) should serialize as {\"tag\":\"WithOneField\",\"name\":\"name\",\"value\":1}" {
             let expected = """{"tag":"WithNamedFields","name":"name","value":1}"""
             let actual = Json.Serialize(WithNamedFields("name", 1.), "tag")
+            Expect.equal actual expected "Expected serializer to convert union case with named fields to object with tag named \"tag\" and named fields"
+        }
+
+        test "TestDU.WithNamedFields(\"name\", 1.) should roundtrip" {
+            let expected = WithNamedFields("name", 1.)
+            let actual = Json.Deserialize(Json.Serialize(expected, "tag"), "tag")
             Expect.equal actual expected "Expected serializer to convert union case with named fields to object with tag named \"tag\" and named fields"
         }
 
@@ -42,9 +97,21 @@ let tests =
             Expect.equal actual expected "Expected serializer to use JsonStringEnumConverter"
         }
 
-        test "TestEnum.First should serialize as \"Single\"" {
+        test "TestEnum.First should roundtrip" {
+            let expected = TestEnum.First
+            let actual = Json.Deserialize(Json.Serialize(expected, "tag"), "tag")
+            Expect.equal actual expected "Expected serializer to use JsonStringEnumConverter"
+        }
+
+        test "TestEnum.Single should serialize as \"Single\"" {
             let expected = "\"Single\""
             let actual = Json.Serialize(TestSingleDU.Single, "tag")
+            Expect.equal actual expected "Expected serializer to convert union case with no fields to string literal"
+        }
+
+        test "TestEnum.Single should roundtrip" {
+            let expected = TestSingleDU.Single
+            let actual = Json.Deserialize(Json.Serialize(expected, "tag"), "tag")
             Expect.equal actual expected "Expected serializer to convert union case with no fields to string literal"
         }
     ]
