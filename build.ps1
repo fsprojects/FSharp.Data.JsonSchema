c06727d5e926ee225fc9de650af54a62c597c586
Moises Nessim <moises.nessim@topmanage.com>
2023-06-06 16:41:50 -0500

    Windows: run tests in net6.0

--- build.ps1
+++ build.ps1
@@ -1,4 +1,4 @@
 dotnet restore
 dotnet build -c Release --no-restore
-dotnet test -c Release --no-restore --no-build --framework net5.0
+dotnet test -c Release --no-restore --no-build --framework net6.0
 dotnet pack -c Release -o bin --no-restore --no-build
